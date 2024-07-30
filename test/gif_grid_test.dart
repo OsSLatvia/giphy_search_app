cc95de17b75b06cb1970a61900005ca293667b4e
OsSLatvia <RTL@inbox.lv>
2024-07-30 03:54:13 +0300

    removed unused import

--- test/gif_grid_test.dart
+++ test/gif_grid_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
